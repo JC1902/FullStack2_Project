90985880302c48c0c1a77efc247e2a8fd4da618d
Charlie00567 <115057211+Charlie00567@users.noreply.github.com>
2024-06-06 13:05:14 -0700

    Cambios en vistas y en archivo docker-compose

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Usuario
 
 # Create your views here.
+class VistaPaginaRegistro( CreateView ):
+    model = Usuario
+    template_name = 'registration/signup.html'
--- proyecto/settings.py
+++ proyecto/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / 'plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
